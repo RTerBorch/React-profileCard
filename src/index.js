612d0dacd5f5d7922554b8fd49939757b3893e91
Robin Ter-Borch <Robin.ter.borch@gmail.com>
2023-12-26 14:22:45 +0100

    profileCard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar(props) {
+  return <img className="avatar" src="/profilePicture.png" alt="Robin" />;
+}
+function Intro(props) {
+  return (
+    <div>
+      <h1>Robin</h1>
+      <p>
+        Hello! This is my profile card, part of my react education. This is a
+        couple of my skills
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <main>
+      <Skill
+        name="JavaScript"
+        emojii={String.fromCodePoint(0x1f44d)}
+        color="blue"
+      />
+      <Skill name="CSS" emojii={String.fromCodePoint(0x1f60a)} color="red" />
+      <Skill
+        name="Java"
+        emojii={String.fromCodePoint(0x1f44d)}
+        color="yellow"
+      />
+      <Skill name="Html" emojii={String.fromCodePoint(0x1f60a)} color="green" />
+    </main>
+  );
+}
+
+function Skill(props) {
+  console.log(props);
+  return (
+    <div className="skill-list" style={{ backgroundColor: props.color }}>
+      <h3>{props.name}</h3>
+      <p>{props.emojii}</p>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
